6e0228d88c9ab0254e7eb5ea561d3d8797617df9
CapitanPNG <pierniccologiannelli@gmail.com>
2020-12-03 10:33:06 +0100

    Initial commit

---
+++ Svelte TS/components/Header.svelte
@@ -0,0 +1,22 @@
+<div class="header">
+    SmartPizza <i class="fas fa-pizza-slice"></i>
+</div>
+
+<style>
+
+.header {
+    width: 100%;
+    height: 60px;
+    padding: 0 10px;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    font-weight: 600;
+    font-size: 18px;
+    color: #ffffff;
+    background-color: #ff5151;
+    box-shadow: 0px 0px 6px 0px #2b2b2b;
+}
+
+</style>
---
+++ Svelte TS/src/App.svelte
@@ -0,0 +1,37 @@
+<script>
+
+import Header from '../components/Header.svelte';
+
+</script>
+
+<svelte:head>
+	<title>SmartPizza</title>
+	<meta charset="utf-8">
+
+	<link rel="stylesheet" href="../resources/fonts/Montserrat/font.css">
+	<link rel="stylesheet" href="../resources/icons/fontawesome-free-5.15.1-web/css/all.css">
+</svelte:head>
+
+<main>
+	<Header />
+</main>
+
+<style>
+
+:global(*) {
+	margin: 0;
+	padding: 0;
+	box-sizing: border-box;
+	outline: none;
+	font-family: "Montserrat";
+}
+
+:global(body) {
+	margin: 0;
+	padding: 0;
+	position: absolute;
+	width: 100%;
+	height: 100%;
+}
+
+</style>
