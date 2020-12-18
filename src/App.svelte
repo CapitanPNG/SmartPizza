214d3ee13bba43ce3e70597f9497803a885f4ce0
CapitanPNG <pierniccologiannelli@gmail.com>
2020-12-18 10:28:16 +0100

    v0

--- src/App.svelte
+++ src/App.svelte
@@ -8,102 +8,52 @@ window.PIZZA = {
 	"Margherita": {
 		"image": "https://static.buttalapasta.it/r/845X0/www.buttalapasta.it/img/Pizza-Margherita-intera.jpg",
 		"price": "4.50",
-		"description": "Pizza description goes here ...",
-		"temp": {
-			"price": "4.50",
-			"quantity": 1,
-			"additionalIngredients": []
-		}
+		"description": "Pizza description goes here ..."
 	},
 	"Margherita con bufala": {
 		"image": "https://www.petitchef.it/imgupl/recipe/pizza-margherita-con-mozzarella-di-bufala--md-457932p714660.jpg",
-		"price": 6.50,
-		"description": "Pizza description goes here ...",
-		"temp": {
-			"price": "4.50",
-			"quantity": 1,
-			"additionalIngredients": []
-		}
+		"price": "6.50",
+		"description": "Pizza description goes here ..."
 	},
 	"Marinara": {
 		"image": "https://wips.plug.it/cips/buonissimo.org/cms/2012/05/pizza-marinara-5.jpg",
-		"price": 4.00,
-		"description": "Pizza description goes here ...",
-		"temp": {
-			"price": "4.50",
-			"quantity": 1,
-			"additionalIngredients": []
-		}
+		"price": "4.00",
+		"description": "Pizza description goes here ..."
 	},
 	"Napoli": {
 		"image": "https://www.funetta.it/_______vecchiowp/wp-content/uploads/2019/08/pizza_capperi_acciughe.jpg",
-		"price": 6.00,
-		"description": "Pizza description goes here ...",
-		"temp": {
-			"price": "4.50",
-			"quantity": 1,
-			"additionalIngredients": []
-		}
+		"price": "6.00",
+		"description": "Pizza description goes here ..."
 	},
 	"4 stagioni": {
 		"image": "https://wips.plug.it/cips/buonissimo.org/cms/2012/05/pizza-alle-quattro-stagioni.jpg?w=712&a=c&h=406",
-		"price": 7.00,
-		"description": "Pizza description goes here ...",
-		"temp": {
-			"price": "4.50",
-			"quantity": 1,
-			"additionalIngredients": []
-		}
+		"price": "7.00",
+		"description": "Pizza description goes here ..."
 	},
 	"Capricciosa": {
 		"image": "https://wips.plug.it/cips/buonissimo.org/cms/2019/09/pizza-capricciosa.jpg",
-		"price": 7.00,
-		"description": "Pizza description goes here ...",
-		"temp": {
-			"price": "4.50",
-			"quantity": 1,
-			"additionalIngredients": []
-		}
+		"price": "7.00",
+		"description": "Pizza description goes here ..."
 	},
 	"4 formaggi": {
 		"image": "https://cdn.tasteatlas.com/images/dishrestaurants/8859616a33b242e7b6b254dde0bd6045.jpg?w=600&h=450",
-		"price": 7.00,
-		"description": "Pizza description goes here ...",
-		"temp": {
-			"price": "4.50",
-			"quantity": 1,
-			"additionalIngredients": []
-		}
+		"price": "7.00",
+		"description": "Pizza description goes here ..."
 	},
 	"Cotto e Porcini": {
 		"image": "https://i0.wp.com/www.piccolericette.net/piccolericette/wp-content/uploads/2019/10/4102_Pizza.jpg?resize=895%2C616&ssl=1",
-		"price": 7.50,
-		"description": "Pizza description goes here ...",
-		"temp": {
-			"price": "4.50",
-			"quantity": 1,
-			"additionalIngredients": []
-		}
+		"price": "7.50",
+		"description": "Pizza description goes here ..."
 	},
 	"Tonno e cipolla": {
 		"image": "https://www.bofrost.ch/writable/products/images-v2/15196.jpg",
-		"price": 5.00,
-		"description": "Pizza description goes here ...",
-		"temp": {
-			"price": "4.50",
-			"quantity": 1,
-			"additionalIngredients": []
-		}
+		"price": "5.00",
+		"description": "Pizza description goes here ..."
 	},
 	"Speck e Mascarpone": {
 		"image": "https://media-cdn.tripadvisor.com/media/photo-s/0b/00/a4/f4/pizza-speck-e-mascarpone.jpg",
-		"price": 8.50,
-		"description": "Pizza description goes here ...",
-		"temp": {
-			"price": "4.50",
-			"quantity": 1,
-			"additionalIngredients": []
-		}
+		"price": "8.50",
+		"description": "Pizza description goes here ..."
 	}
 };
 
